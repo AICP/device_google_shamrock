f4d7ac854a168213aeb2f4d74e7fa66b0112b7ef
Rygebin <kaankulahli@gmail.com>
2016-12-09 19:18:04 +0000

    shamrock: Add audio/media configs to device tree
    * changed correct directory paths for Audio and Media
    
    * add sound trigger
    
    Change-Id: I8948fcc9097ad35392bcbb714f4a25b5fb266964

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -1,15 +1,18 @@
-# Camera
-PRODUCT_PACKAGES += \
-    camera.msm8952 \
-    libmm-qcamera \
-    Snap
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_output_policy.conf:system/etc/audio_output_policy.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_l9300.xml:system/etc/mixer_paths_l9300.xml
+
+# Sound trigger
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
 
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.cpp.duplication=false \
-    persist.camera.hal.debug.mask=0
